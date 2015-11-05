5c998d11b715071d743b223768d4b998f7d30646
Rich Megginson <rmeggins@redhat.com>
2015-11-05 16:47:43 -0700

    first draft of kibana container with elasticsearch support

---
+++ run.sh
@@ -0,0 +1,11 @@
+sed -i "s/es_host/$ES_HOST/" /opt/app-root/src/config/kibana.yml
+sed -i "s/es_port/$ES_PORT/" /opt/app-root/src/config/kibana.yml
+if [ -z "$ES_CA" ] ; then
+    # if not using SSL, change es url to http, and comment out the other SSL stuff
+    sed -i '/^elasticsearch_url:/s/https:/http:/' /opt/app-root/src/config/kibana.yml
+    sed -i 's/^\(kibana_elasticsearch_client_crt.*\)$/# \1/' /opt/app-root/src/config/kibana.yml
+    sed -i 's/^\(kibana_elasticsearch_client_key.*\)$/# \1/' /opt/app-root/src/config/kibana.yml
+    sed -i 's/^\(ca:.*\)$/# \1/' /opt/app-root/src/config/kibana.yml
+fi
+
+kibana
